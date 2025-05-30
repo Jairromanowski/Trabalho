e64f78091214031e861ca6b7e763f722d53f0784
Jairromanowski <164203148+Jairromanowski@users.noreply.github.com>
2025-05-30 09:15:28 -0300

    Delete server.js
    Signed-off-by: Jairromanowski <164203148+Jairromanowski@users.noreply.github.com>

--- server.js
+++
@@ -1,8 +0,0 @@
-import expres from 'express'
-import publiRoutes from './routes/public.js'
-
-const app = express()
-
-app.use('/', publicRoutes)
-
-app.listen(3000, () => console.log("Servidor rodando!!"))
